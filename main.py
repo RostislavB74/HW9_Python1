6e8834ab5bd0797139fa5254cf1f72a0f9d94319
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-03 16:13:14 +0300

    edit

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ def no_command(*args):
 
 
 def get_input(text: str):
-    if text.startswith("hello"):
+    text_lower= text.lower()
+    if text_lower.startswith("hello"):
         return print("How can I help you?")
-    if text.startswith("close"):
+    if text_lower.startswith("close") or text_lower.startswith("exit") or text_lower.startswith("good bye"):
         return "Good bye!"
-
-
-def parser(text: str) -> tuple[callable, tuple[str] | None]:
-    if text.startswith("add"):
+    if text_lower.startswith("add"):
+        print(type(add), type(text.replace("add", "").strip().split()))
         return add, text.replace("add", "").strip().split()
+
+#def parser(text: str): #-> tuple[callable, tuple[str] | None]:
+ #   if text.startswith("add"):
+  #      print (text)
+   #     return add, text.replace("add", "").strip().split()
+    
     # if text.startswith("hello"):
     #     print("How can I help you?")
     #     return
@@ -28,6 +33,7 @@ def main():
     while True:
         user_input = input(">>>")
         command = get_input(user_input)
+        #print (command)
         # command, data = parser(user_input)
         if command == "Good bye!":
             print(command)
