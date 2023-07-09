f0e5ebc7a9f65fbbb97d83dab679882ae9ec24ba
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-09 10:29:40 +0300

    ok

--- ex1.py
+++
@@ -1,9 +0,0 @@
-def multiply(x):
-    def inner(y):
-        def func(z):
-            return x*y*z
-        return func
-    return inner
-
-
-multyply_ten = multiply(10)
--- main -10.py
+++
@@ -1,5 +0,0 @@
-# def add(*args):
-#     name = Name(args[0])
-#     phone = Phone(args[1])
-#     rec = Record(name, phone)
-#     return addressbook.add_rec(rec)
